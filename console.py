1aa7865d833ef8cc9e5d746e04d742ce6e4e45d1
brivan1 <bkryan.bk@gmail.com>
2024-01-23 05:56:38 -0800

    changes on console.py

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ import cmd
 import re
 import ast
 from models.base_model import BaseModel
-from models import storage
+from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
 from models.city import City
